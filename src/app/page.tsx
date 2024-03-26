347401f60392cba8c3dbf6288f75adab7bc7bda7
Walker Finlay <wfinlay@westmonroepartners.com>
2024-03-26 17:19:44 -0400

    testing modify cell contents and read result cell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,50 @@
 
 import styles from "./page.module.css";
 import * as React from "react";
-import { read } from "xlsx";
+import { WorkBook, read, utils } from "xlsx";
 
 const MODEL_LINK = "/model.xlsm";
 
 export default function Home() {
+  const [workbook, setWorkbook] = React.useState<WorkBook>();
+
   React.useEffect(() => {
     fetch(MODEL_LINK)
       .then((res) => res.arrayBuffer())
       .then(read)
-      .then(console.log)
+      .then(setWorkbook)
       .catch(console.error);
   }, []);
+
+  React.useEffect(() => {
+    if (workbook) console.log(workbook);
+  }, [workbook]);
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const tmp = structuredClone(workbook);
+    if (!tmp) return;
+    utils.sheet_add_aoa(tmp.Sheets["Main Page"], [[parseInt(e.target.value)]], {
+      origin: "C9",
+    });
+    setWorkbook(tmp);
+  };
 
   return (
     <main className={styles.main}>
       <h1>online-grocery</h1>
+      <label>
+        Online order size
+        <input
+          type="number"
+          min={0}
+          value={workbook?.Sheets["Main Page"]["C9"].v || 0}
+          onChange={onInputChange}
+        />
+      </label>
+      <label>
+        Base Case Labor Time Per Order (min)
+        <span>{workbook?.Sheets["Main Page"]["C21"].v}</span>
+      </label>
     </main>
   );
 }
