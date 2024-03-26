3ed3521da05651f1c8a30e7fac085105d32784c2
Walker Finlay <wfinlay@westmonroepartners.com>
2024-03-26 15:37:04 -0400

    import and parse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import * as React from "react";
+import { read } from "xlsx";
+
+const MODEL_LINK = "/model.xlsm";
 
 export default function Home() {
+  React.useEffect(() => {
+    fetch(MODEL_LINK)
+      .then((res) => res.arrayBuffer())
+      .then(read)
+      .then(console.log)
+      .catch(console.error);
+  }, []);
+
   return (
     <main className={styles.main}>
       <h1>online-grocery</h1>
+      <table></table>
     </main>
   );
 }
