e6b6081f746e165a7ed549c67e5f94764d89ffa6
Walker Finlay <wfinlay@westmonroepartners.com>
2024-03-27 16:19:04 -0400

    recalculate formulas with xlsx-calc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import styles from "./page.module.css";
 import * as React from "react";
 import { WorkBook, read, utils } from "xlsx";
+const XLSX_CALC = require("xlsx-calc");
 
 const MODEL_LINK = "/model.xlsm";
 
@@ -23,10 +24,17 @@ export default function Home() {
 
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const tmp = structuredClone(workbook);
-    if (!tmp) return;
-    utils.sheet_add_aoa(tmp.Sheets["Main Page"], [[parseInt(e.target.value)]], {
-      origin: "C9",
-    });
+    utils.sheet_add_aoa(
+      tmp!.Sheets["Main Page"],
+      [[parseInt(e.target.value)]],
+      {
+        origin: "C9",
+      }
+    );
+    /* the XLSX_CALC package has a hard time parsing implicit intersection operator but it seems like it's 
+      still working?
+      ex: 'Home Delivery_In Store'!C21 =VLOOKUP(@B:B,'Sub Op Table'!A:C,2,0) throws an error */
+    XLSX_CALC(tmp, { continue_after_error: true });
     setWorkbook(tmp);
   };
 
@@ -34,7 +42,7 @@ export default function Home() {
     <main className={styles.main}>
       <h1>online-grocery</h1>
       <label>
-        Online order size
+        Online order size (C9)
         <input
           type="number"
           min={0}
@@ -43,7 +51,7 @@ export default function Home() {
         />
       </label>
       <label>
-        Base Case Labor Time Per Order (min)
+        Base Case Labor Time Per Order (min) (C21)
         <span>{workbook?.Sheets["Main Page"]["C21"].v}</span>
       </label>
     </main>
