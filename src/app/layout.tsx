335cb58d7478f0c2477f34db38193e3b6d2d4c1f
Walker Finlay <wfinlay@westmonroepartners.com>
2024-04-17 22:09:24 -0400

    layout must define `html` and `body` tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,18 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
-import nextConfig from "../../next.config.mjs";
-import whartonLogo from '../../public/images/Wharton-Logo.png';
+import whartonLogo from "../../public/images/Wharton-Logo.png";
 
-
-import { 
-  ColorSchemeScript, 
-  MantineProvider, 
-  AppShell, 
+import {
+  ColorSchemeScript,
+  MantineProvider,
+  AppShell,
   AppShellHeader,
   AppShellFooter,
-  AppShellSection,
   AppShellMain,
-  Anchor,
-  Group 
+  Group,
 } from "@mantine/core";
-import Image from 'next/image';
+import Image from "next/image";
 
 export const metadata = {
   title: "online-grocery",
@@ -29,38 +25,43 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <ColorSchemeScript/>
-      <MantineProvider>
-        <AppShell
-          header={{ height: 60 }}
-          footer={{ height: 60 }}
-          padding="md"
-        >
-          <AppShellHeader>
-            <Group h="100%" px="md">
-            <Image
-              src={whartonLogo}// Route of the image file
-              height={44} // Desired size with correct aspect ratio
-              width={173} // Desired size with correct aspect ratio
-              alt="Your Name"
-            />
-            </Group>
-          </AppShellHeader>
-          <AppShellMain>
-            {children}
-          </AppShellMain>
-          <AppShellFooter style={{position: "static"}}>
-            <Group h="100%" px="md" justify="center">
-              <span>
-                Built by <a style={{color: "inherit"}} href="https://www.westmonroe.com/" target="_blank">West Monroe</a>
-              </span>
-            </Group>
-          </AppShellFooter>
-        </AppShell>
-      </MantineProvider>
-    </>
-
-    
+    <html lang="en">
+      <body>
+        <ColorSchemeScript />
+        <MantineProvider>
+          <AppShell
+            header={{ height: 60 }}
+            footer={{ height: 60 }}
+            padding="md"
+          >
+            <AppShellHeader>
+              <Group h="100%" px="md">
+                <Image
+                  src={whartonLogo} // Route of the image file
+                  height={44} // Desired size with correct aspect ratio
+                  width={173} // Desired size with correct aspect ratio
+                  alt="Your Name"
+                />
+              </Group>
+            </AppShellHeader>
+            <AppShellMain>{children}</AppShellMain>
+            <AppShellFooter style={{ position: "static" }}>
+              <Group h="100%" px="md" justify="center">
+                <span>
+                  Built by{" "}
+                  <a
+                    style={{ color: "inherit" }}
+                    href="https://www.westmonroe.com/"
+                    target="_blank"
+                  >
+                    West Monroe
+                  </a>
+                </span>
+              </Group>
+            </AppShellFooter>
+          </AppShell>
+        </MantineProvider>
+      </body>
+    </html>
   );
 }
