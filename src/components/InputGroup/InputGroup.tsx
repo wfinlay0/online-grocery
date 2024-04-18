832e0a99e0fa6914b6462cde1e6f3749d6bc2ada
Walker Finlay <wfinlay@westmonroepartners.com>
2024-04-17 23:17:26 -0400

    read data, map to labels, inputs

--- src/components/InputGroup/InputGroup.tsx
+++ src/components/InputGroup/InputGroup.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { WorkBook } from "xlsx";
+import { CellObject, WorkBook, utils } from "xlsx";
 
 interface IInputGroupProps {
   workbook: WorkBook;
@@ -8,6 +8,7 @@ interface IInputGroupProps {
    * a string representing a cell range e.g. `"B9:C14"`
    * - two columns will be interpreted as a column of labels and a column of numbers
    * - one column will be interpreted simply as a column of numbers
+   * - if more than 2 colums, won't throw an error, but only first 2 will be considered
    */
   cellRange: string;
   onSubmit: (content: WorkBook) => void;
@@ -20,7 +21,38 @@ interface IInputGroupProps {
  * @returns
  */
 const InputGroup: React.FunctionComponent<IInputGroupProps> = (props) => {
-  return <div>TODO: InputGroup</div>;
+  const [data, setData] = React.useState<CellObject[][]>();
+
+  React.useEffect(() => {
+    const cr = utils.decode_range(props.cellRange);
+    setData(
+      props.workbook?.Sheets[props.sheet]["!data"]
+        ?.slice(cr.s.r, cr.e.r + 1)
+        .map((row) => row.slice(cr.s.c, cr.e.c + 1))
+    );
+  }, [props.cellRange, props.sheet, props.workbook]);
+
+  const onInputChange = () => {
+    console.error("not yet implemented: TODO: update data (inputgroup state)");
+  };
+
+  return (
+    <div>
+      {data?.map((row, idx) => (
+        <div key={idx}>
+          <label>
+            {utils.format_cell(row[0])}
+            <input
+              type="text"
+              value={utils.format_cell(row[1])}
+              onChange={onInputChange}
+            />
+          </label>
+        </div>
+      ))}
+      <button>submit</button>
+    </div>
+  );
 };
 
 export default InputGroup;
