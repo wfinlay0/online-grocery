1a26fcb6f3c56dff7e52108213931b6d422bd621
Walker Finlay <wfinlay@westmonroepartners.com>
2024-06-19 18:47:13 -0400

    add titles with orders

--- src/components/OutputTable/OutputGroups.tsx
+++ src/components/OutputTable/OutputGroups.tsx
@@ -1,12 +1,27 @@
-import { Box, Table } from "@mantine/core";
+import { Box, Table, Title, TitleOrder } from "@mantine/core";
 import { TSubGroup } from "./OutputTable";
+import { clamp } from "@mantine/hooks";
 
 export interface IOutputGroupsProps {
   data: TSubGroup;
+  /**
+   * What level of the tree we're at. Begins at 0
+   */
+  depth?: number;
 }
 
 const OutputGroups = (props: IOutputGroupsProps) => {
-  return <Box>output groups</Box>;
+  return (
+    <Box>
+      {Array.from(props.data.keys()).map((groupName, idx) => (
+        <Box key={idx}>
+          <Title order={clamp((props.depth ?? 1) + 3, 1, 6) as TitleOrder}>
+            {groupName}
+          </Title>
+        </Box>
+      ))}
+    </Box>
+  );
 };
 
 export default OutputGroups;
